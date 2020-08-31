05b152bb39ee77a627c4e4e90f24a570fdc696c0
Dimitar Markov <dmarkov@seibert-media.net>
2020-08-31 16:46:38 +0200

    Add Navigation Bar, Footer, Router, all main pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AboutComponent } from './about/about.component';
+import { SightsComponent } from './sights/sights.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    GalleryComponent,
+    ContactComponent,
+    ReservationComponent,
+    HomeComponent,
+    AboutComponent,
+    SightsComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [MenuComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
