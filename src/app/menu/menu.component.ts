12c0cf0ab39a1dadeee6dd5bfd270f600105abe9
Dimitar Markov <dmarkov@seibert-media.net>
2020-12-03 19:30:08 +0100

    Change navigation bar buttons

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faPaw, faPhone, faNewspaper, faUsers, faCameraRetro} from '@fortawesome/free-solid-svg-icons';
+import { faPaw, faDonate, faNewspaper, faUsers, faCameraRetro} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -7,7 +7,7 @@ import { faPaw, faPhone, faNewspaper, faUsers, faCameraRetro} from '@fortawesome
 })
 export class MenuComponent implements OnInit {
   pawIcon = faPaw;
-  phoneIcon = faPhone;
+  donateIcon = faDonate;
   newspaperIcon = faNewspaper;
   usersIcon = faUsers;
   cameraIcon = faCameraRetro;
