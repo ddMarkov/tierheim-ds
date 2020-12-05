0e0896a434b75ed666bc244a11bd8ebb4161f761
Dimitar Markov <dmarkov@seibert-media.net>
2020-12-05 23:09:19 +0100

    v.1.1

--- src/app/animals/animals.component.ts
+++ src/app/animals/animals.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-animals',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./animals.component.css']
 })
 export class AnimalsComponent implements OnInit {
-
+  searchIcon = faSearch;
   constructor() { }
 
   ngOnInit(): void {
